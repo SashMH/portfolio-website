fdf5edc6e613b376548b6cd3368a1cd1643a0642
SashMH <48863401+SashMH@users.noreply.github.com>
2019-04-22 20:34:33 +0100

    Java Script first initiation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Welcome to my portfolio :)")
+/*console.log("Welcome to my portfolio :)")*/
+
+function menuToggle() {
+  var x = documentElementById("myNavtoggle");
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
