1358a575b9845637de5a3c24eb952f4648d0f1e2
SashMH <48863401+SashMH@users.noreply.github.com>
2019-04-24 23:18:43 +0100

    Correcting CSS & JS for menue

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /*console.log("Welcome to my portfolio :)")*/
 
 function menuToggle() {
-  var x = documentElementById("myNavtoggle");
+  var x = document.getElementById("myNavtoggle");
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
